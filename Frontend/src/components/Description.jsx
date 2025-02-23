20239053dd8b74068d300e7eb92925616f3b6d31
Mehak Singla <singlamehak2005@gmail.com>
2025-02-24 00:26:05 +0530

    again a change in design

--- Frontend/src/components/Description.jsx
+++ Frontend/src/components/Description.jsx
@@ -7,17 +7,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Im
 const Description = () => {
   return (
     <section className="component" id='description' style={{ backgroundColor: '#d1f0ed' }}>
-      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
-        <div style={{textAlign : 'left', marginLeft : '2%', display: 'flex', gap: '20px', flexWrap: 'wrap' , marginRight : '2%'}}>
-          <div className='flex items-center justify-center'><h2>From Symptons to Solutions-</h2></div>
-          <h2>DigiCare Got You Covered!</h2>
-          <p> An AI-powered diagnostic assistant designed to help healthcare professionals make faster,more accurate diagnosis.Analyze medical images ,patient data and symptoms with unparalleled efficiency.</p>
-          <Link to="/appointments"><button>Book Now</button></Link>
+     <div className="flex flex-col items-center justify-center text-center flex-grow px-6">
+         
+          <h1 className="text-5xl font-bold leading-tight mb-4">
+          From Symptons to Solutions-<br />
+         DigiCare Got You Covered!
+        </h1>
+        <p className="text-gray-400 max-w-xl mb-6">
+                    An AI-powered diagnostic assistant designed to help healthcare professionals make faster,more accurate diagnosis.Analyze medical images ,patient data and symptoms with unparalleled efficiency.</p>
+                 <Link to="/appointments"><button>Book Now</button></Link>
         </div>
         {/* <div style={{ display: 'flex', gap: '20px', flexWrap: 'wrap'}}>
           <img src={docnew} alt="Doctor Image" style={{maxwidth : '100%', width : '680px'}} />
         </div>  */}
-      </div>
+     
     </section>
   ); 
 };
