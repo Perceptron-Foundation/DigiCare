f4f3c509b150a0b2ae40aa2649afdda6ae272ef3
PinakGupta <pinakgupta99@gmail.com>
2025-04-09 18:45:23 +0530

    Models- doctor snd patient

--- Backend/models/doctor.js
+++ Backend/models/doctor.js
@@ -1,30 +1,82 @@
 const mongoose = require('mongoose');
 
 const doctorSchema = new mongoose.Schema({
-    auth0Id:{
-        type : String,
-        required : true
+    auth0Id: {
+        type: String,
+        required: true
     },
-    name: {
-        type : String,
-        required : true
+    fullName: {
+        type: String,
+        required: true
     },
-    age: {
-        type : Number,
-        required : true
+    gender: {
+        type: String,
+        enum: ['Male', 'Female', 'Other'],
+        required: true
     },
-    bloodGroup : {
-        type : String,
-        
+    dateOfBirth: {
+        type: Date,
+        required: true
     },
+    profilePhoto: {
+        type: String, // URL to the avatar
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phoneNumber: {
+        type: String,
+        required: true
+    },
+    clinicAddress: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    state: {
+        type: String,
+        required: true
+    },
+    country: {
+        type: String,
+        required: true
+    },
+    availableHours: {
+        type: String,
+        required: true
+    },
+    timezone: {
+        type: String,
+        required: true
+    },
+    registrationNumber: {
+        type: String,
+        required: true
+    },
+    specializations: [{
+        type: String,
+        required: true
+    }],
+    yearsOfExperience: {
+        type: Number,
+        required: true
+    },
+    degrees: [{
+        type: String,
+        required: true
+    }],
     patients: [{
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'patient'
+        ref: 'Patient'
     }],
     summary: [{
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'summary'
+        ref: 'Summary'
     }]
-},{ timestamps: true })
+}, { timestamps: true });
 
 module.exports = mongoose.model('Doctor', doctorSchema);
--- Backend/models/patient.js
+++ Backend/models/patient.js
@@ -1,42 +1,75 @@
 const mongoose = require('mongoose');
 
 const patientSchema = new mongoose.Schema({
-    auth0Id:{
-        type : String,
-        required : true
+    auth0Id: {
+        type: String,
+        required: true
     },
-    name: {
-        type : String,
-        required : true
+    profilePhoto: {
+        type: String, // URL to the avatar
+    },
+    fullName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phoneNumber: {
+        type: String,
+        required: true
+    },
+    dateOfBirth: {
+        type: Date,
+        required: true
+    },
+    gender: {
+        type: String,
+        enum: ['Male', 'Female', 'Other'],
+        required: true
     },
     age: {
-        type : Number,
-        required : true
+        type: Number,
+        required: true
     },
-    bloodGroup : {
-        type : String,
-        required : true,
+    maritalStatus: {
+        type: String,
+        enum: ['Single', 'Married', 'Divorced', 'Widowed'],
+        required: true
     },
-    familyHistry: {
-        type : Number,
-        required : true
+    bloodGroup: {
+        type: String,
+        enum: ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'],
+        required: true
     },
-    priorMed: {
-        type : Number,
-        required : true
+    address: {
+        type: String,
+        required: true
+    },
+    medicalHistory: {
+        type: String,
+        required: true
+    },
+    currentMedications: {
+        type: String
+    },
+    familyMedicalHistory: {
+        type: String,
+        required: true
     },
     reports: [{
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'report'
+        ref: 'Reports'
     }],
-    doctor: [{
+    doctors: [{
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'doctor'
+        ref: 'Doctor'
     }],
-    summary : [{
-        type : mongoose.Schema.Types.ObjectId,
-        ref : 'summary'
+    summary: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Summary'
     }]
-},{ timestamps: true })
+}, { timestamps: true });
 
 module.exports = mongoose.model('Patient', patientSchema);
