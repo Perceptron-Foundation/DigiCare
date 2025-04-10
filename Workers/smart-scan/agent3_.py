e8130a509374a4eaeb889ac6b73d5c15673797a4
therishabhmittal-05 <rishabhmittal928@gmail.com>
2025-04-10 03:06:52 +0000

    Agent1, Agent2, Agnet3 functions done

---
+++ Workers/smart-scan/agent3_.py
@@ -0,0 +1,8 @@
+from reportlab.lib.pagesizes import A4
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
+from reportlab.lib.enums import TA_CENTER
+from reportlab.lib.colors import HexColor
+import os
+
+
